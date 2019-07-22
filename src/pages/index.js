c7ab01a2abe7cdf913f0582cc43113a5afa54470
Tomasz <tomkowalczyk@hotmail.com>
2019-07-22 18:29:51 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../fonts/BentonSans Regular.ttf';
-import '../fonts/BentonSans Light.ttf';
-import '../fonts/BentonSans Thin.ttf';
+import '../fonts/BentonSansRegular.ttf';
+import '../fonts/BentonSansLight.ttf';
+import '../fonts/BentonSansThin.ttf';
 import Header from '../components/Header';
 import 'scss/bwk.scss'
 // import { offers } from '../source';
