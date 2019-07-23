43627bac5c1587e27ac6c96a5f8cac5a0f5abe2f
Tomasz <tomkowalczyk@hotmail.com>
2019-07-23 17:19:57 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import Header from '../components/Header';
 import 'scss/bwk.scss'
-import { offers } from '../source';
+import Advantage from '../components/Advantage';
+import Headline from '../components/Headline';
+import location from '../images/icon-location.png';
+import aircon from '../images/icon-aircon.png';
+import coffee from '../images/icon-coffee.png';
+import internet from '../images/icon-internet.png';
 
 const IndexPage = () => (
   <main>
-    <Header/>
-    <h2>home</h2>
-    
+
+    <div className='container'>
+      <div className="container">
+        <Headline title="Korzyści" />
+        <div className="advantage-section">
+          <Advantage
+            icon={location}
+            title="Prestiżowa lokalizacja"
+            content="Biura i lokale położone w wiekowej, nowoczesnie odnowionej kamienicy"
+          />
+          <Advantage
+            icon={aircon}
+            title="Klimatyzacja"
+            content="Każde pomieszczenie wyposażone jest w klimatyzację"
+          />
+          <Advantage
+            icon={coffee}
+            title="Kącik socjalny"
+            content="Wspólne pomieszczenie wyposażone w ekspres do kawy, czajnik, mikrofalówkę"
+          />
+          <Advantage
+            icon={internet}
+            title="Szybki internet"
+            content="Internet wi-fi dostępny we wszystkich biurach w cenie wynajmu"
+          />
+        </div>
+      </div>
+    </div>
+
   </main>
 );
 export default IndexPage
