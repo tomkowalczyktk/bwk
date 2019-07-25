3e73e923987fe5e125c069835bb6b709b8992171
Tomasz <tomkowalczyk@hotmail.com>
2019-07-25 12:28:29 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import 'scss/bwk.scss'
 // import Advantage from '../components/Advantage';
 import Headline from '../components/Headline';
-import location from '../images/icon-location.png';
-import aircon from '../images/icon-aircon.png';
-import coffee from '../images/icon-coffee.png';
-import internet from '../images/icon-internet.png';
 import AdvantageSection from '../components/AdvantageSection';
 
 const IndexPage = () => (
