dfad550ff584e3ce07241834cae90106afbf8483
Tomasz <tomkowalczyk@hotmail.com>
2019-07-25 12:59:37 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import 'scss/bwk.scss'
 import HeaderHeadline from '../components/HeaderHeadline';
-import Advantage from '../components/Advantage';
-import location from '../images/icon-location.png';
-import internet from '../images/icon-internet.png';
-import aircon from '../images/icon-aircon.png';
-import coffee from '../images/icon-coffee.png';
-import '../components/AdvantageSection/advantage-section.scss'
-import Headline from '../components/Headline/Headline';
-import Offices from '../components/Offices/Offices';
-import Testimonial from '../components/Testimonial/Testimonial';
 import HeaderButton from '../components/HeaderButton';
 import keyImage from '../images/key.png';
 
