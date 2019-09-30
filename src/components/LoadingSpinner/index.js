e955a8ad42fdb9a022ceacb83238d9c90b010381
Tomasz <tomkowalczyk@hotmail.com>
2019-10-01 01:11:06 +0200

    Page not found

--- src/components/LoadingSpinner/index.js
+++ src/components/LoadingSpinner/index.js
@@ -1,11 +1,58 @@
 import React from 'react';
 import './spinner.scss';
+import styled from 'styled-components';
 
-export default function LoadingSpinner() {
+const Spinner = styled.div`
+  display: flex;
+  flex-direction: column;
+  &__img {
+    display: inline-block;
+    margin: auto;
+    width: 60px;
+    height: 60px;
+    background-color: lightcyan;
+    background-image: url("../../images/logo-mobile.png");
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    position: relative;
+    border-radius: 50%;
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      border: 2px dashed black;
+      animation: mymove 5s infinite linear;
+
+      @keyframes mymove {
+        0% {
+          transform: rotate(0deg) scale(1);
+          box-shadow: 2px 0 10px ${({ color }) => color};
+        }
+        50% {
+          transform: rotate(180deg) scale(1.3);
+          box-shadow: 2px 0 20px ${({ color }) => color};
+        }
+        100% {
+          transform: rotate(360deg) scale(1);
+          box-shadow: 2px 0 10px ${({ color }) => color};
+        }
+      }
+    }
+  }
+
+`
+
+
+export default function LoadingSpinner({ color }) {
   return (
-    <div className='spinner'>
+    <Spinner color={color}>
       <div className='spinner__img'></div>
-      <h5 className='spinner__text'>Loading...</h5>
-    </div>
+
+    </Spinner>
   )
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import LoadingSpinner from '../components/LoadingSpinner'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSadCry } from '@fortawesome/free-solid-svg-icons';
+
+
+
+const Main = styled.main`
+margin:0;
+background-color: black;
+height: 100vh;
+width: 100%;
+`
+
+const H1 = styled.h1`
+color:red;
+font-weight:800;
+opacity:.4;
+text-align:center;
+margin:60px auto;
+position:relative;
+::before {
+  content:'Nie znaleziono takiej strony!!!';
+  position:absolute;
+  top:2px;
+  margin-left:2px;
+  color:rgba(0 0 0 .4);
+}
+`
+const Sign = styled.div`
+width: 60px;
+height: 60px;
+text-justify:center;
+text-align:center;
+margin:100px auto;
+svg{
+  font-size:60px; 
+  color:#f7d317;
+  margin: 70px auto;
+}
+`
+const StyledLink = styled.div`
+font-size:18px;
+color:blue;
+font-weight:600;
+font-style:italic;
+display:flex;
+justify-content:center;
+margin:70px auto;
+:hover{
+  text-shadow: 2px 0  5px;
+}
+`
+
+
+
+
+
 const NotFoundPage = () => (
-  <main>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </main>
+  <Main>
+    <Sign ><FontAwesomeIcon icon={faSadCry} /></Sign>
+    <H1>Nie znaleziono takiej strony!!!</H1>
+    <LoadingSpinner color="red" />
+
+    <StyledLink as={Link} to='/'>Zabierz mnie do strony głównej.</StyledLink>
+  </Main>
 )
 
 export default NotFoundPage
